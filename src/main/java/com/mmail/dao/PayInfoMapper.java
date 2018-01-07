f7bc95943dd3394270bb1fbe21887b263cb4479a
geguofeng <849447894@qq.com>
2018-01-08 00:00:07 +0800

    mybatis generator

---
+++ src/main/java/com/mmail/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.mmail.dao;
+
+import com.mmail.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
